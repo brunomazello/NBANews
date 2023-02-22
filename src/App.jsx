5a8846d2f4242c202cbb3768595bf08a21e9cdad
Bruno <bruno.rmazello@gmail.com>
2023-02-22 19:37:23 -0300

    Projeto iniciado com React e Styled Components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import styled from "styled-components";
+
+function App() {
+  return (
+    <div>
+      <h1>Meu Piru é grandãooo</h1>
+    </div>
+  );
+}
+
+export default App;
